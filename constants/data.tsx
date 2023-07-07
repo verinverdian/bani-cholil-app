a03643822fad4716da8079aaec37f990b54e71a3
Verdian Desya <verdian@tees.co.id>
2023-07-07 09:51:31 +0700

    constants: added data

---
+++ constants/data.tsx
@@ -0,0 +1,138 @@
+export const MEMBERS = [
+    {
+        nib: "0601",
+        name: "Kamonah",
+        img: "/images/members/",
+        position: "Anak Ke-1",
+        spouse: "",
+        profession: "-",
+        address: "Jl Kopral Sapari Jepara",
+        child: [
+            {
+                nib: "",
+                name: "",
+                img: "/images/members/",
+                position: "",
+                spouse: "",
+                profession: "",
+                address: "",
+                child: [ 
+                    {
+                        nib: "",
+                        name: "",
+                        img: "/images/members/",
+                        position: "",
+                        spouse: "",
+                        profession: "",
+                        address: "",
+                        child: [
+                            {
+
+                            }
+                        ]
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        nib: "0602",
+        name: "Husin",
+        img: "/images/members/",
+        position: "Anak Ke-2",
+        spouse: "",
+        profession: "-",
+        address: "Jl Kopral Sapari Jepara",
+        child: [
+            {
+                nib: "",
+                name: "",
+                img: "/images/members/",
+                position: "",
+                spouse: "",
+                profession: "",
+                address: "",
+                child: [ 
+                    {
+                        nib: "",
+                        name: "",
+                        img: "/images/members/",
+                        position: "",
+                        spouse: "",
+                        profession: "",
+                        address: "",
+                        child: [
+                            {
+
+                            }
+                        ]
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        nib: "0603",
+        name: "Nur",
+        img: "/images/members/",
+        position: "Anak Ke-3",
+        spouse: "",
+        profession: "-",
+        address: "Jl Kopral Sapari Jepara",
+        child: [
+            {
+                nib: "060301",
+                name: "Etty",
+                img: "/images/members/",
+                position: "Anak Ke-1",
+                spouse: "",
+                profession: "",
+                address: "",
+                child: [ 
+                    {
+                        nib: "",
+                        name: "",
+                        img: "/images/members/",
+                        position: "",
+                        spouse: "",
+                        profession: "",
+                        address: "",
+                        child: [
+                            {
+
+                            }
+                        ]
+                    }
+                ]
+            }
+        ]
+    }
+
+]  
+
+export const ALBUMS = [
+    {
+        name: "",
+        img: "/images/albums/",
+        desc: "",
+        alt: ""
+    },
+    {
+        name: "",
+        img: "/images/albums/",
+        desc: "",
+        alt: ""
+    },
+    {
+        name: "",
+        img: "/images/albums/",
+        desc: "",
+        alt: ""
+    },
+] 
+
+export const PROFESSIONS = [
+    {
+
+    }
+]
